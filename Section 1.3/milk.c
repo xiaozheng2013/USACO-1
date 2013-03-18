4c1e2be494596c1f6fc53417f4a6f493648dd28c
Yiting <yli229@uic.edu>
2013-03-18 14:03:17 -0500

    Update milk.c

--- Section 1.3/milk.c
+++ Section 1.3/milk.c
@@ -1,74 +1,113 @@
 /*
 ID: Yiting
 LANG: C
-TASK: milk
+TASK: barn1
  */
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#define MAX 5000
+#define MAX 200
+#define POSITION 1
+#define VALUE 2
 
-typedef struct PA {
-    int P;
-    int A;
-} pa;
+typedef struct PV {
+    int position;
+    int value;
+} pv;
 
-void Exchange(pa *P1, pa *P2) {
-    int p, a;
-    p = P1->P;
-    a = P1->A;
-    P1->P = P2->P;
-    P1->A = P2->A;
-    P2->P = p;
-    P2->A = a;
+void Exc(void* p1, void* p2, int size) {
+    char buf[size];
+    memcpy(buf, p1, size);
+    memcpy(p1, p2, size);
+    memcpy(p2, buf, size);
 }
 
-int Partition(pa P[MAX], int p, int r) {
-    int x = P[r].P, i = p - 1, j;
+int Partition(pv P[MAX], int p, int r, int k) {
+    int x = (k == 1) ? P[r].position : P[r].value, i = p - 1, j;
     for (j = p; j < r; j++) {
-        if (P[j].P < x) {
+        int temp = (k == 1) ? P[j].position : P[j].value;
+        if (temp > x) {
             i++;
             if (i != j)
-                Exchange(&P[i], &P[j]);
+                Exc(&P[i], &P[j], sizeof (pv));
         }
     }
     if (i + 1 != r)
-        Exchange(&P[i + 1], &P[r]);
+        Exc(&P[i + 1], &P[r], sizeof (pv));
     return i + 1;
 }
 
-void Quicksort(pa P[MAX], int p, int r) {
+void Quicksort(pv P[MAX], int p, int r, int k) {
     if (p < r) {
-        int q = Partition(P, p, r);
-        Quicksort(P, p, q - 1);
-        Quicksort(P, q + 1, r);
+        int q = Partition(P, p, r, k);
+        Quicksort(P, p, q - 1, k);
+        Quicksort(P, q + 1, r, k);
+    }
+}
+
+int Partition1(int P[MAX], int p, int r) {
+    int x = P[r], j, i = p - 1;
+    for (j = p; j < r; j++) {
+        if (P[j] < x) {
+            i++;
+            if (i != j)
+                Exc(&P[i], &P[j], sizeof (int));
+        }
+    }
+    if (i + 1 != r)
+        Exc(&P[i + 1], &P[r], sizeof (int));
+    return i + 1;
+}
+
+void Quicksort1(int P[MAX], int p, int r) {
+    if (p < r) {
+        int q = Partition1(P, p, r);
+        Quicksort1(P, p, q - 1);
+        Quicksort1(P, q + 1, r);
     }
 }
 
 main() {
-    FILE* fin = fopen("milk.in", "r");
-    FILE* fout = fopen("milk.out", "w");
+    FILE *fin = fopen("barn1.in", "r");
+    FILE *fout = fopen("barn1.out", "w");
 
-    int N, M, i, number = 0, amount = 0;
-    pa P[MAX];
-    fscanf(fin, "%d %d", &N, &M);
-    for (i = 0; i < M; i++) {
-        fscanf(fin, "%d %d", &P[i].P, &P[i].A);
+    int M, S, C, i, total = 0;
+    int stall[MAX];
+    pv adjacent[MAX - 1];
+    fscanf(fin, "%d %d %d", &M, &S, &C);
+    if (M >= C) {
+        fprintf(fout, "%d\n", C);
+        exit(0);
     }
-    //first, sort by Price
-    Quicksort(P, 0, M - 1);
+    for (i = 0; i < C; i++)
+        fscanf(fin, "%d", &stall[i]);
+    //sort the stall first
+    Quicksort1(stall, 0, C - 1);
 
-    //then scan from the first set of data
-    for (i = 0; i < M; i++) {
-        if (number + P[i].A <= N) {
-            number = number + P[i].A;
-            amount = amount + P[i].P * P[i].A;
-        } else {
-            amount = amount + P[i].P * (N - number);
-            break;
-        }
+    if (M == 1) {
+        fprintf(fout, "%d\n", stall[C - 1] - stall[0] + 1);
+        exit(0);
     }
-    fprintf(fout, "%d\n", amount);
+    for (i = 0; i < C - 1; i++) {
+        adjacent[i].position = i;
+        adjacent[i].value = stall[i + 1] - stall[i];
+    }
+    //find the 1st, 2nd, 3rd ... Mth number in array adjacent 
+    pv temp[MAX - 1], result[MAX - 1];
+    memcpy(temp, adjacent, sizeof (adjacent));
+    Quicksort(temp, 0, C - 2, VALUE);
+    memcpy(result, temp, sizeof (pv)*(M - 1));
+    Quicksort(result, 0, M - 2, POSITION);
+
+    for (i = M - 2; i >= 0; i--) {
+        if (i == M - 2)
+            total = total + stall[result[i].position] - stall[0] + 1;
+        else
+            total = total + stall[result[i].position] - stall[result[i + 1].position + 1] + 1;
+        if (i == 0)
+            total = total + stall[C - 1] - stall[result[i].position + 1] + 1;
+    }
+    fprintf(fout, "%d\n", total);
     exit(0);
 }
